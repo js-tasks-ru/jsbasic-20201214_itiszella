bea6b4c30940283fbab925c792bcea01d011bb97
Roman Shalupenko <itiszella@gmail.com>
2020-12-19 14:11:37 +0300

    truncate a string

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,8 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
+  let newStr = '';
+  if (str.length > maxlength) return newStr = str.slice(0, maxlength - 1) + "…";
+  return str;
   // ваш код...
 }
