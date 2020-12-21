60d090c05371099bdf6b3a9894aacbe675900870
Roman Shalupenko <itiszella@gmail.com>
2020-12-21 11:22:39 +0300

    work on bugs 5 task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -7,8 +7,8 @@
 function truncate(str, maxlength) {
 
   if (str.length > maxlength) {
-    return str.slice(0, maxlength - 1) + '…';
+    return `${str.slice(0, maxlength - 1) + '…'}`;
   }
 
-return str;
+ return str;
 }
