13b1456afff350d9174ce12c1ee53ad00d708160
Roman Shalupenko <itiszella@gmail.com>
2020-12-22 17:26:49 +0300

    module 2 task 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,17 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (const key in salaries) {
+    if (isNaN(salaries[key]) || salaries[key] == Infinity || salaries[key] == -Infinity) {
+       delete salaries[key];
+    } else if (typeof salaries[key] === 'number') {
+      sum += salaries[key];
+    }
+   
+  }
+   return sum;
 }
+
+
+sumSalary(salaries)
