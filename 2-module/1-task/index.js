37e720d199d70309b85bc3ba7ba2ca81de5281d6
Roman Shalupenko <itiszella@gmail.com>
2020-12-23 17:32:13 +0300

    module 2 task 1 work on bugs

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -6,10 +6,14 @@
 function sumSalary(salaries) {
   let sum = 0;
   let clone = Object.assign({}, salaries);
+
   for (const key in clone) {
-  if (typeof clone[key] === 'number' && Number.isFinite(clone[key])) {
+
+    if (typeof clone[key] === 'number' && Number.isFinite(clone[key])) {
       sum += clone[key];
     }
+    
   }
-   return sum;
+
+  return sum;
 }
