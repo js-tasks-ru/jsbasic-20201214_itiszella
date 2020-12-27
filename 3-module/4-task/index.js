333458f88e2b39e28c7a58f42f1432c889f939bd
Roman Shalupenko <itiszella@gmail.com>
2020-12-27 23:55:40 +0300

    modul 3 task 4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -4,6 +4,10 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
-function showSalary(users, age) {
-  // ваш код...
+
+function showSalary(users, age){
+
+  let res = users.filter(item => item.age <= age);
+   
+  return res.map((item, index) => (index == res.length - 1)  ? `${item.name}, ${item.balance}`  : `${item.name}, ${item.balance}\n`).join('');
 }
