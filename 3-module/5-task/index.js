4ec39933362136e763d5ab81b9d142d0a4f721af
Roman Shalupenko <itiszella@gmail.com>
2020-12-28 10:10:27 +0300

    modul 3 task 5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,17 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+
+   let result = {};
+
+   let res = str.split(' ').join(',').split(',').filter(function(number) {
+
+       return !isNaN(Number(number));
+   });
+
+   result.min = Math.min(...res);
+   result.max = Math.max(...res);
+   
+   return result;
 }
+
