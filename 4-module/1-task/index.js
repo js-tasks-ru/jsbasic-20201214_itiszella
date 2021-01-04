0b8822a37091b546806337342f27940ca204a2f2
Roman Shalupenko <itiszella@gmail.com>
2021-01-04 15:38:50 +0300

    modul 4 task 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,16 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  
+  for (i = 0; i < friends.length; i++) {
+
+    let li = document.createElement('li');
+    li.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+    ul.appendChild(li);
+  }
+
+  document.body.appendChild(ul);
+  return ul;
+  
 }
