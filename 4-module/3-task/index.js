b271dc5711327494029335b3b70c97c215f2fe33
Roman Shalupenko <itiszella@gmail.com>
2021-01-04 17:32:23 +0300

    modul 4 task 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,37 @@
  * @param {Element} table
  */
 function highlight(table) {
-}
+    console.log(table);
+    
+    let tbody = table.getElementsByTagName('tbody')[0];
+    let trs = tbody.getElementsByTagName('tr');
+  
+      [...trs].forEach((element) => {
+
+              let status = element.getElementsByTagName('td')[3];
+
+              if(!status.dataset.available) {
+                  element.hidden = true;
+              }
+              else if(status.dataset.available === "true") {
+                  element.classList.add('available');
+              } else {
+                element.classList.add('unavailable');
+              }
+        
+              let gender = element.getElementsByTagName('td')[2];
+
+              if (gender.textContent === 'm') {
+                  element.classList.add('male');
+              } else {
+                element.classList.add('female');
+              }
+
+              let age = element.getElementsByTagName('td')[1];
+
+              if (Number(age.textContent) < 18) {
+                element.style.textDecoration = 'line-through';
+              }
+          }
+      );
+  }
